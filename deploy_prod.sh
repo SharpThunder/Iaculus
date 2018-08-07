926ce02f19d80b1907c675d3c38713253bee6e1b
sharpthunder <snnoztrk95@hotmail.com>
2018-08-07 14:29:20 +0000

    jenkins

---
+++ deploy_prod.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ssh sharpthunder@35.237.233.134 <<EOF
+  cd Iaculus
+  git pull
+  source /home/sharpthunder/Iaculus/.venv/bin/activate
+  pip install -r requirements.txt
+  python ./Iaculus/manage.py migrate
+  sudo supervisorctl restart iaculus
+  exit
+
